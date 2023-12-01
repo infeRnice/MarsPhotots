f6cc630e73ada7845355b1382aa28f0e54b346e1
infernice <infernice92@gmail.com>
2023-12-01 16:57:12 +0800

    Retrofit with Moshi added

---
+++ app/src/main/java/network/MarsApi.kt
@@ -0,0 +1,25 @@
+package network
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+
+private const val BASE_URL =
+    "https://android-kotlin-fun-mars-server.appspot.com"
+
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
+
+object MarsApi {
+    val retrofitService : MarsApiService by lazy {
+        retrofit.create(MarsApiService::class.java)
+    }
+}
---
+++ app/src/main/java/network/MarsApiService.kt
@@ -0,0 +1,8 @@
+package network
+
+import retrofit2.http.GET
+
+interface MarsApiService {
+    @GET("photos")
+    suspend fun getPhotos(): List<MarsPhoto>
+}
